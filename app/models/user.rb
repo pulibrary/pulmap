bc33158f9d4d1d1f4b49cd45701fd81d71acb056
Eliot Jordan <eliotj@princeton.edu>
2014-11-21 17:33:45 -0500

    add updates for CAS devise auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,30 @@
 class User < ActiveRecord::Base
 
-  attr_accessible :email, :password, :password_confirmation if Rails::VERSION::MAJOR < 4
 # Connects this user object to Blacklights Bookmarks. 
   include Blacklight::User
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :cas_authenticatable, :rememberable, :trackable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
   # the account.
   def to_s
     email
+  end
+
+  def default_values
+    self.username ||= self.username
+    self.email ||= "#{self.username}@princeton.edu"
+  end
+
+  def cas_extra_attributes=(extra_attributes)
+    extra_attributes.each do |name, value|
+      case name.to_sym
+      when :email
+        self.email = value
+      end
+    end
   end
 end
--- db/migrate/20141121210905_add_username_to_users.rb
+++ db/migrate/20141121210905_add_username_to_users.rb
@@ -1,7 +1,11 @@
 class AddUsernameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :username, :string, :null => false
-
+  def self.up
+    add_column :users, :username, :string
+    change_column  :users, :username, :string, :null => false
     add_index :users, :username, :unique => true
+  end
+
+  def self.down
+  	remove_column :users, :username, :string
   end
 end
