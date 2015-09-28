3235396a701d09e3282b949bd92eb37fb4af9b2d
Eliot Jordan <eliotj@princeton.edu>
2015-09-28 15:36:51 -0400

    improve map style and behavior

---
+++ app/assets/javascripts/modules/map.js
@@ -0,0 +1,22 @@
+// modify map viewer behaviors
+
+GeoBlacklight.Viewer.Map = GeoBlacklight.Viewer.Map.extend({
+
+  // options for leaflet map
+  mapOptions: {
+    scrollWheelZoom: false,
+  },
+
+  // override load function to add map options
+  load: function() {
+    if (this.data.mapBbox) {
+      this.options.bbox = L.bboxToBounds(this.data.mapBbox);
+    }
+    this.map = L.map(this.element, this.mapOptions ).fitBounds(this.options.bbox);
+    this.map.addLayer(this.selectBasemap());
+    this.map.addLayer(this.overlay);
+    if (this.data.map !== 'index') {
+      this.addBoundsOverlay(this.options.bbox);
+    }
+  },
+});
