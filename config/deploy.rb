b6d6c64bdce95e9d453608edabbb912e07f26888
Eliot Jordan <eliotj@princeton.edu>
2015-10-05 18:49:25 -0400

    fix javascript assets pipeline

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,6 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-
 set :application, 'pulmap'
 set :repo_url, 'git@github.com:pulibrary/pulmap.git'
 set :branch, 'master'
