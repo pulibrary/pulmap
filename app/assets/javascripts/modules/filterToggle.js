8aff6fd32ca39126f9f20472d7ca6b031106e13b
Axa Mei Liauw <aliauw@princeton.edu>
2015-10-20 16:55:09 -0400

    Remove shame (comment out first)

--- app/assets/javascripts/modules/filterToggle.js
+++ app/assets/javascripts/modules/filterToggle.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $(".facets-toggler").click(function(){
         $("#facets.collapse").collapse('toggle');
+        $(this).text(function(i,old){
+            return old=='More Filters' ?  'Less Filters' : 'More Filters';
+        });
     });
 
     $("#map-container").css({'height':($("#main-container").height())});
