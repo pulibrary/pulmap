d701fff2060c365c7d8d76cca38513ab561159ae
Axa Mei Liauw <aliauw@princeton.edu>
2015-10-23 13:59:51 -0400

    Add access facet to search bar as placeholder for other commonly used facet

--- app/assets/javascripts/modules/filterToggle.js
+++ app/assets/javascripts/modules/filterToggle.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     $(".facets-toggler").click(function(){
         $("#facets.collapse").collapse('toggle');
-        $(this).text(function(i,old){
+        $(".text").text(function(i,old){
             return old=='More Filters' ?  'Less Filters' : 'More Filters';
         });
+        $(".facets-toggler .glyphicon").toggleClass('glyphicon-triangle-bottom glyphicon-triangle-right');
     });
 
     $("#map-container").css({'height':($("#main-container").height())});
