8f6a54e2acfa16856fe2afc434dce577d7e7f374
Eliot Jordan <eliotj@princeton.edu>
2016-01-18 15:40:58 -0500

    rename unused method argument

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,9 +20,9 @@ class User < ActiveRecord::Base
     username
   end
 
-  def self.find_for_cas(access_token, signed_in_resource=nil)
+  def self.find_for_cas(access_token, _signed_in_resource=nil)
     logger.debug "#{access_token.inspect}"
-  
+    
     @user = User.where(provider: access_token.provider, uid: access_token.uid).first_or_create do |user|
       user.uid = access_token.uid
       user.username = access_token.uid
