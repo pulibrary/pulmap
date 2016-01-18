ee9b36625a10b92c89bd86318758b98809b95ec7
Eliot Jordan <eliotj@princeton.edu>
2016-01-18 16:21:24 -0500

    fix spacing issues

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,7 +11,7 @@ module Users
     def find_user(auth_type)
       find_method = "find_for_#{auth_type.downcase}".to_sym
       $stderr.puts "#{auth_type} :: #{current_user.inspect}"
-      @user = User.send(find_method,request.env["omniauth.auth"], current_user)
+      @user = User.send(find_method, request.env["omniauth.auth"], current_user)
       if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: auth_type
         sign_in_and_redirect @user, event: :authentication
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,14 @@ class User < ActiveRecord::Base
     username
   end
 
-  def self.find_for_cas(access_token, _signed_in_resource=nil)
+  def self.find_for_cas(access_token, _signed_in_resource = nil)
     logger.debug "#{access_token.inspect}"
     
     @user = User.where(provider: access_token.provider, uid: access_token.uid).first_or_create do |user|
       user.uid = access_token.uid
       user.username = access_token.uid
       user.email = "#{access_token.uid}@princeton.edu" 
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
       user.provider = access_token.provider
     end
     @user
