6c61834254d5418a8fa2d4772a3289a9486469fa
Eliot Jordan <eliotj@princeton.edu>
2016-01-18 16:14:13 -0500

    use parens with percent literals

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -10,7 +10,7 @@ set :rails_env, 'development'
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'libruby-dev', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+server 'libruby-dev', user: 'deploy', roles: %w(web app db), my_property: :my_value
 
 # role-based syntax
 # ==================
