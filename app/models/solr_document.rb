c294a36a30e80415fb4f4fc671aec568e2146104
Eliot Jordan <eliotj@princeton.edu>
2016-01-18 16:10:44 -0500

    fix indentation

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class SolrDocument 
   include Blacklight::Solr::Document
- include Geoblacklight::SolrDocument
+  include Geoblacklight::SolrDocument
 
   # self.unique_key = 'id'
   self.unique_key = 'layer_slug_s'
