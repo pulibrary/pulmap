50969307219736c22bd49f4b4c2d4649be886a5d
Axa Mei Liauw <aliauw@princeton.edu>
2016-01-20 12:41:40 -0500

    Fix map and facet views. Remove bootstrap grid for sidebar and content in results.

--- app/assets/javascripts/modules/filterToggle.js
+++ app/assets/javascripts/modules/filterToggle.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-  $('.map-view').hide();
+
+  $('#map-container').hide();
   $('#map-toggle').on('click', function() {
-    $('#facets .facet-view, #facets .map-view').toggle();
+    $('#facets .facet-view, #facets #map-container').toggle();
     $('.map-toggle').text(function(i, old) {
       return old === 'Turn Map On' ? 'Turn Map Off' : 'Turn Map On';
     });
+    $('#side-content').toggleClass('map-view');
+    $('#content').toggleClass('map-view');
   });
 });
