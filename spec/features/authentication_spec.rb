75d1b819373979dd35864cf4802028610b5d9e43
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2016-06-29 17:19:06 -0400

    refers user back to current page after login via cas

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -6,15 +6,10 @@ RSpec.feature 'Authentication', type: :feature do
 
     before do
       OmniAuth.config.test_mode = true
-      referer = catalog_path 'columbia-columbia-landinfo-global-aet'
-      Capybara.current_session.driver.header 'Referer', referer
+      visit catalog_path 'columbia-columbia-landinfo-global-aet'
       sign_in(user)
     end
     scenario 'after login, user returns to last visited page' do
-      expect(page).to have_css '.blacklight-catalog-show', count: 1
-    end
-    scenario 'after logout, user returns to last visited page' do
-      visit '/users/sign_out'
       expect(page).to have_css '.blacklight-catalog-show', count: 1
     end
   end
