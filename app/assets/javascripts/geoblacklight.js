0ea4804cb6824c85887fafea9ec139d53d221623
Eliot Jordan <eliot.jordan@gmail.com>
2016-07-29 14:40:28 -0500

    change way that javascript modules are overridden (#172)

--- app/assets/javascripts/geoblacklight.js
+++ app/assets/javascripts/geoblacklight.js
@@ -2,7 +2,11 @@
 //= require geoblacklight/basemaps
 //= require geoblacklight/controls
 //= require geoblacklight/viewers
-//= require geoblacklight/modules
+//= require geoblacklight/modules/collapse
+//= require geoblacklight/modules/download
+//= require geoblacklight/modules/item
+//= require geoblacklight/modules/layer_opacity
+//= require geoblacklight/modules/util
 //= require geoblacklight/downloaders
 
 //= require leaflet-iiif
