459da9039cb00125e31d317b57d815b948843e3a
Eliot Jordan <eliotj@princeton.edu>
2016-09-19 18:30:53 -0400

    use mapzen who's on first instead of google geosearch

--- app/assets/javascripts/modules/home.js
+++ app/assets/javascripts/modules/home.js
@@ -1,5 +1,15 @@
 Blacklight.onLoad(function() {
   $('[data-map="home"]').each(function(i, element) {
     GeoBlacklight.Home = new GeoBlacklight.Viewer.Map(this);
+    L.control.geocoder('search-gczeV3H', {
+      placeholder: 'Find location',
+      markers: true,
+      pointIcon: false,
+      polygonIcon: false,
+      expanded: true,
+      params: {
+        sources: ['whosonfirst']
+      }
+    }).addTo(GeoBlacklight.Home.map);
   });
 });
