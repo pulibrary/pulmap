0630e51e6ef3426acd5e81826a9285bb5dda1e2d
Eliot Jordan <eliot.jordan@gmail.com>
2016-10-28 16:49:18 -0500

    Add settings yaml to capistrano linked files (#204)
    * add settings yaml to capistrano linked files
    
    * remove byebug_history

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,8 @@ set :pty, true
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/blacklight.yml',
                                                  'config/database.yml',
-                                                 'config/secrets.yml')
+                                                 'config/secrets.yml',
+                                                 'config/settings.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log',
