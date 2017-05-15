ece463b77f249c984647de7140d7ab7c367dc176
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2017-05-15 17:03:19 -0400

    support targeted search fields

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -166,39 +166,49 @@ class CatalogController < ApplicationController
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields.
 
-    # config.add_search_field('title') do |field|
-    #   # solr_parameters hash are sent to Solr as ordinary url query params.
-    #   field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+    config.add_search_field('all_fields') do |field|
+      field.qt = 'search'
+      field.label = 'Keyword'
+      field.solr_local_parameters = {
+        qf: '$qf',
+        pf: '$pf'
+      }
+    end
 
-    #   # :solr_local_parameters will be sent using Solr LocalParams
-    #   # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-    #   # Solr parameter de-referencing like $title_qf.
-    #   # See: http://wiki.apache.org/solr/LocalParams
-    #   field.solr_local_parameters = {
-    #     :qf => '$title_qf',
-    #     :pf => '$title_pf'
-    #   }
-    # end
+    config.add_search_field('title') do |field|
+      # solr_parameters hash are sent to Solr as ordinary url query params.
+      # field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
 
-    # config.add_search_field('author') do |field|
-    #   field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
-    #   field.solr_local_parameters = {
-    #     :qf => '$author_qf',
-    #     :pf => '$author_pf'
-    #   }
-    # end
+      # :solr_local_parameters will be sent using Solr LocalParams
+      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
+      # Solr parameter de-referencing like $title_qf.
+      # See: http://wiki.apache.org/solr/LocalParams
+      field.solr_local_parameters = {
+        qf: '$title_qf',
+        pf: '$title_pf'
+      }
+    end
 
-    # # Specifying a :qt only to show it's possible, and so our internal automated
-    # # tests can test it. In this case it's the same as
-    # # config[:default_solr_parameters][:qt], so isn't actually neccesary.
-    # config.add_search_field('subject') do |field|
-    #   field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
-    #   field.qt = 'search'
-    #   field.solr_local_parameters = {
-    #     :qf => '$subject_qf',
-    #     :pf => '$subject_pf'
-    #   }
-    # end
+    config.add_search_field('publisher') do |field|
+      field.qt = 'search'
+      field.label = 'Publisher/Creator'
+      field.solr_local_parameters = {
+        qf: '$publisher_qf',
+        pf: '$publisher_pf'
+      }
+    end
+
+    # Specifying a :qt only to show it's possible, and so our internal automated
+    # tests can test it. In this case it's the same as
+    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
+    config.add_search_field('subject') do |field|
+      # field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+      field.qt = 'search'
+      field.solr_local_parameters = {
+        qf: '$subject_qf',
+        pf: '$subject_pf'
+      }
+    end
 
     #  config.add_search_field('Institution') do |field|
     #   field.solr_parameters = { :'spellcheck.dictionary' => 'Institution' }
---
+++ solr/README.md
@@ -0,0 +1,8 @@
+## Important
+
+The files in this directory are not versioned.  They are downloaded from
+https://github.com/pulibrary/pul_solr/tree/master/solr_configs/pulmap/conf using the rake task:
+
+```
+rake pulmap:solr:update
+```
