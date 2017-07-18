dd74d482aec26877233e9ce32d76130fdd6ded0d
Eliot Jordan <eliotj@princeton.edu>
2017-07-18 10:22:59 -0500

    Add fullscreen map to home page

--- app/assets/javascripts/modules/home.js
+++ app/assets/javascripts/modules/home.js
@@ -1,7 +1,15 @@
 Blacklight.onLoad(function() {
   $('[data-map="home"]').each(function(i, element) {
-    var bbox;
-    bbox = L.latLngBounds([[77, 180],[-47, -180]]);
-    GeoBlacklight.Home = new GeoBlacklight.Viewer.Map(this, { bbox: bbox });
+    var bbox, indexMap;
+	bbox = L.latLngBounds([[47.2, -5.6],[49.1, 6.9]]);
+    indexMap = new GeoBlacklight.Viewer.Map(this, { bbox: bbox });
+
+    // Remove zoon control
+    indexMap.map.zoomControl.removeFrom(indexMap.map)
+
+    // Disable interactivity
+    indexMap.map._handlers.forEach(function(handler) {
+      handler.disable();
+	});
   });
 });
