4703476da9bd6723c057b97ffde8516299039e78
Axa Mei Liauw <aliauw@princeton.edu>
2017-07-18 09:44:46 -0400

    Redesign home screen. Closes #308

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,14 @@ module ApplicationHelper
   ##
   # Checks if index is the current layout
   # @return [Boolean]
+  def home_layout?
+    layout_type == 'home'
+  end
+
+>>>>>>> Redesign home screen. Closes #308
+  ##
+  # Checks if index is the current layout
+  # @return [Boolean]
   def index_layout?
     layout_type == 'index'
   end
@@ -28,7 +36,7 @@ module ApplicationHelper
       if params[:action] == 'show' || params[:action] == 'downloads'
         'item'
       elsif params[:action] == 'index'
-        'index'
+        has_search_parameters? ? 'index' : 'home'
       end
     else
       'default'
