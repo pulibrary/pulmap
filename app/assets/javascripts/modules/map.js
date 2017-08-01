b82181ca51c6c5494967ba569b693b254b65a766
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2017-08-01 12:16:20 -0400

    fix bounding box overlay

--- app/assets/javascripts/modules/map.js
+++ app/assets/javascripts/modules/map.js
@@ -4,7 +4,7 @@ GeoBlacklight.Viewer.Map = GeoBlacklight.Viewer.Map.extend({
 
   // override initial bbox
   options: {
-    bbox: [[0, -20],[40, 20]]
+    bbox: [[-30, -80],[70, 0]]
   },
 
   // options for leaflet map
