9a1ed159fc9e1bebcec322bb806a30e0c177290d
Shaun Ellis <shaun@sdellis.com>
2017-08-03 17:08:23 -0400

    fixes footer issues with index and other actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
+  layout :resolve_layout
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -9,6 +10,15 @@ class ApplicationController < ActionController::Base
   before_action :store_current_location, unless: :devise_controller?
 
   private
+
+  def resolve_layout
+    case action_name
+    when "index"
+      "index"
+    else
+      "application"
+    end
+  end
 
   def store_current_location
     store_location_for(:user, request.url)
