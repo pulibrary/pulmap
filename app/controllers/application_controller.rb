0aca57ca7b134ea006bcf884dca498c30be6fb46
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2017-08-07 21:54:52 -0400

    single application layout template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
-  layout :resolve_layout
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -10,15 +9,6 @@ class ApplicationController < ActionController::Base
   before_action :store_current_location, unless: :devise_controller?
 
   private
-
-  def resolve_layout
-    case action_name
-    when "index"
-      "index"
-    else
-      "application"
-    end
-  end
 
   def store_current_location
     store_location_for(:user, request.url)
