8519763a64f66c27a3a3fecec48c7cf96f0cd048
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2017-08-08 18:35:04 -0500

    Results and map 50/50 layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,6 @@ module ApplicationHelper
   ##
   # Checks if index is the current layout
   # @return [Boolean]
-  def home_layout?
-    layout_type == 'home'
-  end
-
-  ##
-  # Checks if index is the current layout
-  # @return [Boolean]
   def index_layout?
     layout_type == 'index'
   end
@@ -27,6 +20,22 @@ module ApplicationHelper
     layout_type == 'default'
   end
 
+  def container_type
+    if index_layout?
+      'container-fluid'
+    else
+      'container'
+    end
+  end
+
+  def navbar_type
+    if index_layout?
+      'navbar-fixed-top'
+    else
+      'navbar'
+    end
+  end
+
   ##
   # Gets current layout for use in rendering partials
   # @return [String] item, index, home, or default
@@ -35,7 +44,7 @@ module ApplicationHelper
       if params[:action] == 'show' || params[:action] == 'downloads'
         'item'
       elsif params[:action] == 'index'
-        has_search_parameters? ? 'index' : 'home'
+        'index'
       end
     else
       'default'
