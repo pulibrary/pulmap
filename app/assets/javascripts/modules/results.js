e6678688f4b58a421561cb7209428572fd191604
Eliot Jordan <eliotj@princeton.edu>
2017-08-10 15:30:05 -0500

    Place document markers on the results map

--- app/assets/javascripts/modules/results.js
+++ app/assets/javascripts/modules/results.js
@@ -1,28 +1,4 @@
 Blacklight.onLoad(function() {
-  function updatePage(url) {
-    $.get(url).done(function(data) {
-      var resp = $.parseHTML(data);
-      $doc = $(resp);
-      $('#documents').replaceWith($doc.find('#documents'));
-      $('#index-pagination').replaceWith($doc.find('#index-pagination'));
-      $('#sortAndPerPage').replaceWith($doc.find('#sortAndPerPage'));
-      $('#facet-view').replaceWith($doc.find('#facet-view'));
-      if ($('#appliedParams').length) {
-        $('#appliedParams').replaceWith($doc.find('#appliedParams'));
-      } else {
-        $('#content').prepend($doc.find('#appliedParams'));
-      }
-      if ($('#map').next().length) {
-        $('#map').next().replaceWith($doc.find('#map').next());
-      } else {
-        $('#map').after($doc.find('#map').next());
-      }
-
-      // reload thumbnail images
-      aload();
-    });
-  }
-
   var historySupported = !!(window.history && window.history.pushState);
 
   if (historySupported) {
@@ -35,55 +11,112 @@ Blacklight.onLoad(function() {
   $('[data-map="index"]').each(function(i, element) {
     var data = $(this).data(),
         opts = { baseUrl: data.catalogPath }.
-        bbox;
+        bbox = initialBoundingBox(data);
 
-    var lngRe = '(-?[0-9]{1,3}(\\.[0-9]+)?)';
-    var latRe = '(-?[0-9]{1,2}(\\.[0-9]+)?)';
+    // Instantiate new map
+    GeoBlacklight.Home = new GeoBlacklight.Viewer.Map(this, { bbox: bbox });
 
-    var parseableBbox = new RegExp(
-      [lngRe,latRe,lngRe,latRe].join('\\s+')
-    );
+    // Remove initial overlay
+    GeoBlacklight.Home.removeBoundsOverlay();
+
+    // Add geosearch control
+    L.control.geosearch(opts).addTo(GeoBlacklight.Home.map);
+
+    // Set document markers
+    placeMarkers();
+
+    // Set hover listeners
+    setHoverListeners();
+  });
+
+  function initialBoundingBox(data) {
+    var world = L.latLngBounds([[-90, -180], [90, 180]]);
+    var filter_params = L.Control.GeoSearch.prototype.filterParams();
 
     if (typeof data.mapBbox === 'string') {
       bbox = L.bboxToBounds(data.mapBbox);
     } else {
       $('.document [data-bbox]').each(function() {
-        var currentBbox = $(this).data().bbox;
 
-        if (parseableBbox.test(currentBbox)) {
-          if (typeof bbox === 'undefined') {
-            bbox = L.bboxToBounds(currentBbox);
-          } else {
-            bbox.extend(L.bboxToBounds(currentBbox));
+        try {
+          var currentBounds = L.bboxToBounds($(this).data().bbox);
+          if (!world.contains(currentBounds)) {
+            throw "Invalid bounds";
           }
-        } else {
-          // bbox not parseable, use default value.
-          L.bboxToBounds("-180 -90 180 90");
+          if (typeof bbox === 'undefined') {
+            bbox = currentBounds;
+          } else {
+            bbox.extend(currentBounds);
+          }
+        } catch (e) {
+          bbox = L.bboxToBounds("-180 -90 180 90");
         }
       });
     }
-
-    var filter_params = L.Control.GeoSearch.prototype.filterParams();
 
     // Set bbox to default value if bbox is empty or there are no filter params
     if ((typeof bbox === 'undefined') || (filter_params.length === 0)) {
       bbox = L.latLngBounds([[-20, -100],[45, 100]]);
     }
 
-    GeoBlacklight.Home = new GeoBlacklight.Viewer.Map(this, { bbox: bbox });
+    return bbox;
+  }
 
-    // Remove initial overlay
-    GeoBlacklight.Home.removeBoundsOverlay();
-    L.control.geosearch(opts).addTo(GeoBlacklight.Home.map);
+  function placeMarkers() {
+    // Clear existing markers
+    GeoBlacklight.Home.removeMarkers();
 
-    // set hover listeners on map
-    $('#content')
-      .on('mouseenter', '#documents [data-layer-id]', function() {
-        var bounds = L.bboxToBounds($(this).data('bbox'));
-        GeoBlacklight.Home.addBoundsOverlay(bounds);
-      })
-      .on('mouseleave', '#documents [data-layer-id]', function() {
-        GeoBlacklight.Home.removeBoundsOverlay();
+    $('.document [data-bbox]').each(function() {
+      var currentBbox = $(this).data().bbox,
+          counter = $(this).data().counter + 1,
+          redMarker = L.ExtraMarkers.icon({
+            innerHTML: '<p style="color: white; margin-top: 8px;">' + counter + '</p>',
+            markerColor: 'blue',
+            shape: 'square',
+            prefix: 'fa'
+          });
+
+      if (currentBbox) {
+        var bounds = L.bboxToBounds(currentBbox);
+        L.marker(bounds.getCenter(), {icon: redMarker}).addTo(GeoBlacklight.Home.markers);
+      }
+    });
+  }
+
+  function setHoverListeners() {
+    $('[data-map="index"]').each(function(i, element) {
+      $('#content')
+        .on('mouseenter', '#documents [data-layer-id]', function() {
+          var bounds = L.bboxToBounds($(this).data('bbox'));
+          GeoBlacklight.Home.addBoundsOverlay(bounds);
+        })
+        .on('mouseleave', '#documents [data-layer-id]', function() {
+          GeoBlacklight.Home.removeBoundsOverlay();
       });
-  });
+    });
+  }
+
+  function updatePage(url) {
+    $.get(url).done(function(data) {
+      var resp = $.parseHTML(data);
+      $doc = $(resp);
+
+      // Reload content
+      $('#content').replaceWith($doc.find('#content'));
+
+      // Append hidden bbox seach input to improve push state search behavior
+      if ($('form.search-query-form.form-inline.home-search.navbar-form').find('input[name=bbox]').length) {
+        $('input[name=bbox]')[0].replaceWith($doc.find('input[name=bbox]')[0])
+      } else {
+        $('form.search-query-form.form-inline.home-search.navbar-form').prepend($doc.find('input[name=bbox]')[0])
+      }
+
+      // Reload thumbnail images
+      aload();
+
+      // Reload markers and listeners
+      placeMarkers();
+      setHoverListeners();
+    });
+  }
 });
