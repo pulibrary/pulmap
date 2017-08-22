dc98d970f1fa68607d2f23e1a4460e470f8f8990
Eliot Jordan <eliotj@princeton.edu>
2017-08-21 23:17:40 -0500

    Hide constraints when query parameter is empty

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,6 +36,24 @@ module ApplicationHelper
     end
   end
 
+  def hide_constraints?
+    skip_params = constraint_params_to_skip
+    skip_params << 'q' if params[:q] && params[:q].empty?
+    params.reject { |p| skip_params.include?(p) }.empty?
+  end
+
+  def constraint_params_to_skip
+    %w(
+      page
+      per_page
+      search_field
+      sort
+      controller
+      action
+      utf8
+    )
+  end
+
   ##
   # Gets current layout for use in rendering partials
   # @return [String] item, index, home, or default
