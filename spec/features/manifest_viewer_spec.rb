6852a8f1ad798bee1ee908a44e071a1b64c2cd2a
jrgriffiniii@gmail.com <jrg5@princeton.edu>
2017-08-25 18:37:38 -0400

    Resolves #391 by avoiding the embedding of the UniversalViewer, and inserts two instances as a work-around to disable the hash URI parameters
    Resolving styling issues for the helper method
    
    Updating the manifest_viewer Feature spec. for the updated DOM structure

--- spec/features/manifest_viewer_spec.rb
+++ spec/features/manifest_viewer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'IIIF Universal Viewer' do
   scenario 'renders for record that includes IIIF manifest from Plum' do
     visit solr_document_path('princeton-8c97ks94m')
-    expect(page).to have_css('#view')
+    expect(page).to have_css('.view.uv')
   end
 
   scenario 'does not render record titles' do
