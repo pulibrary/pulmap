ce8ed81407d30bd94e3938c59cd3ff8dc372999d
Eliot Jordan <eliotj@princeton.edu>
2017-08-30 14:00:07 -0500

    Add featured content parameter to search builder

---
+++ app/helpers/constraints_helper.rb
@@ -0,0 +1,15 @@
+module ConstraintsHelper
+  def render_constraints_filters(localized_params = params)
+    content = super(localized_params)
+    localized_params = localized_params.to_unsafe_h unless localized_params.is_a?(Hash)
+
+    if localized_params[:featured]
+      value = localized_params[:featured].humanize.split.map(&:capitalize).join(' ')
+      path = search_action_path(remove_spatial_filter_group(:featured, localized_params))
+      content << render_constraint_element(t('pulmap.search.filters.featured_label'),
+                                           value, remove: path)
+    end
+
+    content
+  end
+end
