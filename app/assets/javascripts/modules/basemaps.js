4b4ff7f2b4fef830d8f4794fe36059f0993e2a29
Eliot Jordan <eliotj@princeton.edu>
2017-10-13 11:53:41 -0400

    Cleanup unneeded css, javascript, and views

--- app/assets/javascripts/modules/basemaps.js
+++ app/assets/javascripts/modules/basemaps.js
@@ -1,42 +1,4 @@
 // additional leaflet base layers
-
-GeoBlacklight.Basemaps.outdoors = L.tileLayer(
-  'https://api.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWxqb3JkYWNoZSIsImEiOiIyNWVkMzk3Zjg4ODM1MDY0MzFmNzYwNjA3NzIzOTgzZSJ9.4EgS3PeSYEhPLy_CaCExSQ', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-    maxZoom: 18,
-    worldCopyJump: false,
-    detectRetina: true,
-    continuousWorld: false,
-    noWrap: true
-  }
-);
-
-GeoBlacklight.Basemaps.mapboxLite = L.tileLayer(
-  'https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IlhHVkZmaW8ifQ.hAMX5hSW-QnTeRCMAy9A8Q&update=iepyi', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-    maxZoom: 18,
-    worldCopyJump: true,
-    detectRetina: true
-  }
- );
-
-GeoBlacklight.Basemaps.antique = L.tileLayer(
-  'https://cartocdn_{s}.global.ssl.fastly.net/base-antique/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-    maxZoom: 18,
-    worldCopyJump: true,
-    detectRetina: true
-  }
-);
-
-GeoBlacklight.Basemaps.positron = L.tileLayer(
-  'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-    worldCopyJump: true,
-    detectRetina: true
-  }
-);
-
 GeoBlacklight.Basemaps.esri =  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
   minZoom: 1,
@@ -45,4 +7,3 @@ GeoBlacklight.Basemaps.esri =  L.tileLayer('https://server.arcgisonline.com/ArcG
   detectRetina: true,
   noWrap: false
 });
-
