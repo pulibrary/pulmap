8f73e6dca26ec94dc4c75b5ed0ff670be6159248
Eliot Jordan <eliotj@princeton.edu>
2017-10-26 17:14:37 -0500

    Add alt attribute to thumbnail image

--- spec/helpers/thumbnail_helper_spec.rb
+++ spec/helpers/thumbnail_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe ThumbnailHelper, type: :helper do
   let(:document) { SolrDocument.new(document_attributes) }
-  let(:document_attributes) { { layer_geom_type_s: 'Polygon' } }
+  let(:document_attributes) { { dc_title_s: 'title', layer_geom_type_s: 'Polygon' } }
 
   describe '#gbl_thumbnail_img_tag' do
     context 'document has a thumbnail url' do
@@ -13,6 +13,7 @@ describe ThumbnailHelper, type: :helper do
       it 'returns a geoblacklight icon span and an image tag' do
         html = Capybara.string(helper.gbl_thumbnail_img_tag(document))
         expect(html).to have_xpath("//img[@data-aload='http://www.example.com/image.jpg']")
+        expect(html).to have_xpath("//img[contains(@alt,'title')]")
         expect(html).to have_xpath("//span[contains(@class,'geoblacklight-polygon')]")
       end
     end
