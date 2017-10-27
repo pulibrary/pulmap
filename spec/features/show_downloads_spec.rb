f85db134dbda3ac2d57dcbe350adea8b90331308
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2017-10-27 15:07:03 -0400

    Render link to contact us form for princeton items unavailable online (#450)

--- spec/features/show_downloads_spec.rb
+++ spec/features/show_downloads_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'Shapefile (and derivative) Downloads' do
+feature 'Show page downloads' do
   scenario 'renders the the link to the download credit request page for Princeton Documents' do
     visit solr_document_path('princeton-8c97ks94m')
     expect(page).to have_css '#downloadsLink'
@@ -9,6 +9,11 @@ feature 'Shapefile (and derivative) Downloads' do
   scenario 'renders the the authentication link for access-restricted Princeton Documents' do
     visit solr_document_path('princeton-3x816p22x')
     expect(page).to have_link 'Login to view and download'
+  end
+
+  scenario 'renders a link to request offline access for Princeton Documents unavailable online' do
+    visit solr_document_path('princeton-fk4bk1p41f')
+    expect(page).to have_link 'Request offline access'
   end
 
   scenario 'renders the panel containing the download links for non-Princeton Documents' do
