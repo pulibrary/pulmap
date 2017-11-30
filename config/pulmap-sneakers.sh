67d796dd6a3c369460fb6d5b8846a7421e95ee1a
Eliot Jordan <eliotj@princeton.edu>
2017-11-30 13:25:18 -0600

    Update settings for new staging server

--- config/pulmap-sneakers.sh
+++ config/pulmap-sneakers.sh
@@ -2,6 +2,6 @@
 
 # Logs out to /var/log/upstart/sidekiq.log by default
 source /home/deploy/.bashrc
-cd /opt/rails_app/current
+cd /opt/pulmap/current
 export RAILS_ENV=production
 exec bundle exec rake sneakers:run WORKERS=GeoblacklightEventHandler RAILS_ENV=production > log/sneakers.log
