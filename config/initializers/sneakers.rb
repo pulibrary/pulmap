b87b9620aeba2e81ad5b6fee0d72bfef41e3725e
Eliot Jordan <eliotj@princeton.edu>
2018-03-29 14:22:14 -0500

    Add geoserver sync worker

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,9 +1,20 @@
 require 'sneakers'
+require 'sneakers/handlers/maxretry'
 require_relative 'pulmap_config'
 Sneakers.configure(
   amqp: Pulmap.config["events"]["server"],
   exchange: Pulmap.config["events"]["exchange"],
   exchange_type: :fanout,
-  daemonize: true
+  handler: Sneakers::Handlers::Maxretry
 )
 Sneakers.logger.level = Logger::INFO
+
+WORKER_OPTIONS = {
+  ack: true,
+  threads: 5,
+  prefetch: 10,
+  timeout_job_after: 60,
+  heartbeat: 5,
+  amqp_heartbeat: 10,
+  retry_timeout: 60 * 1000 # 60 seconds
+}.freeze
