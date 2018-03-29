b1f702fd25038d40f96a356fe68e3ebb35995d65
Eliot Jordan <eliotj@princeton.edu>
2018-03-29 14:22:14 -0500

    Add geoserver sync worker

--- config/pulmap-sneakers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Logs out to /var/log/upstart/sidekiq.log by default
-source /home/deploy/.bashrc
-cd /opt/pulmap/current
-export RAILS_ENV=production
-exec bundle exec rake sneakers:run WORKERS=GeoblacklightEventHandler RAILS_ENV=production > log/sneakers.log
