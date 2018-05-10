c872f051aaac41619302cc024cc19b1ee673a121
Eliot Jordan <eliotj@princeton.edu>
2018-05-10 12:09:43 -0500

    Show thumbnail on show page for restricted content

---
+++ spec/features/show_page_metadata_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'Metadata display on show page' do
+  context 'with a public scanned map record' do
+    it 'renders default metadata' do
+      visit solr_document_path 'princeton-02870w62c'
+      expect(page).to have_css '.blacklight-dc_creator_sm', text: 'Author(s):'
+      expect(page).to have_css '.blacklight-dc_publisher_s', text: 'Publisher:'
+      expect(page).to have_css '.blacklight-dc_rights_s', text: 'Access:'
+      expect(page).to have_css '.blacklight-dc_description_s', text: 'Description:'
+      expect(page).to have_css '.blacklight-dct_spatial_sm', text: 'Place(s):'
+      expect(page).to have_css '.blacklight-dc_subject_sm', text: 'Subject(s):'
+      expect(page).to have_css '.blacklight-dct_temporal_sm', text: 'Year:'
+      expect(page).to have_css '.blacklight-dct_provenance_s', text: 'Held by:'
+      expect(page).not_to have_content 'Thumbnail:'
+    end
+  end
+
+  context 'with a restricted scanned map record with a thumbnail url' do
+    it 'renders a thumbnail' do
+      visit solr_document_path 'princeton-kk91fn37z'
+      expect(page).to have_content 'Thumbnail:'
+    end
+  end
+
+  context 'with a restricted scanned map record without a thumbnail url' do
+    it 'does not render a thumbnail' do
+      visit solr_document_path 'princeton-fk4bk1p41f'
+      expect(page).not_to have_content 'Thumbnail:'
+    end
+  end
+end
