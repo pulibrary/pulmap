bbf588cabbe16a2f3a2db7b5fbe96fa161c52900
Eliot Jordan <eliotj@princeton.edu>
2018-10-12 15:12:20 -0500

    Remove redundant login button on restricted content

---
+++ spec/features/show_page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe 'Show page' do
+  context 'with a restricted Princeton scanned map' do
+    it 'the login to view and download button is not displayed' do
+      visit solr_document_path 'princeton-kk91fn37z'
+      expect(page).not_to have_content 'Login to view and download'
+    end
+  end
+end
