8b19e23ec6d60adc19e02e1e326768b7bc91f8ad
Eliot Jordan <eliotj@princeton.edu>
2018-10-12 14:44:20 -0500

    Set sqlite represent_boolean_as_integer to true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818021612) do
+ActiveRecord::Schema.define(version: 2015_08_18_021612) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
