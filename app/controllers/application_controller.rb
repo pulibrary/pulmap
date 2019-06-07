da2523d2de5784948714a8612fab63b059163afb
Eliot Jordan <eliotj@princeton.edu>
2019-06-07 15:34:24 -0500

    remove discard_flash_if_xhr action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
 
-  skip_after_action :discard_flash_if_xhr
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
