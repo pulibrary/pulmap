c8c1329c625b8c721828ebbdb4b7e98bf50ddf6b
Eliot Jordan <eliotj@princeton.edu>
2019-06-10 16:53:51 -0500

    Remove SearchHelper from ThumbnailsController

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -1,17 +1,25 @@
 class ThumbnailsController < ApplicationController
-  include Blacklight::SearchHelper
-
   def index
     send_data thumbnail[:data], type: thumbnail[:type], disposition: 'inline'
   end
 
   private
 
+    # Replace with Blacklight::Searchable when BL version is >= 7.1
+    def search_service
+      Blacklight::SearchService.new(config: blacklight_config, user_params: search_state.to_h, **search_service_context)
+    end
+
+    # Replace with Blacklight::Searchable when BL version is >= 7.1
+    def search_service_context
+      {}
+    end
+
     def thumbnail
       if Rails.cache.exist?("thumbnails/#{params[:id]}")
         Rails.cache.read("thumbnails/#{params[:id]}")
       else
-        _response, @document = fetch params[:id]
+        _response, @document = search_service.fetch params[:id]
         CacheThumbnailJob.perform_later(@document.to_h)
         placeholder
       end
