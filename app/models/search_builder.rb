1f89ab9b7455299525f03229195db4f967f26d08
Eliot Jordan <eliotj@princeton.edu>
2019-06-28 10:53:43 -0500

    Remove hide_suppressed_records method because it is now in GeoBlacklight

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -7,13 +7,5 @@ class SearchBuilder < Blacklight::SearchBuilder
   include Geoblacklight::SpatialSearchBehavior
   include ::FeaturedContentBehavior
 
-  self.default_processor_chain += %i[add_advanced_parse_q_to_solr add_advanced_search_to_solr
-                                     hide_suppressed_records add_featured_content]
-
-  def hide_suppressed_records(solr_params)
-    # Show child records if searching for a specific source parent
-    return unless blacklight_params.fetch(:f, {})[:dc_source_sm].nil?
-    solr_params[:fq] ||= []
-    solr_params[:fq] << '-suppressed_b: true'
-  end
+  self.default_processor_chain += %i[add_advanced_parse_q_to_solr add_advanced_search_to_solr add_featured_content]
 end
