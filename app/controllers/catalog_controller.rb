062c1500af7bb18ac0d047276ec9cac6e8703718
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2019-07-30 15:16:08 -0400

    updates catalog controller solr params to work with 7.7

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -181,32 +181,28 @@ class CatalogController < ApplicationController
     config.add_search_field('all_fields') do |field|
       field.qt = 'search'
       field.label = 'Keyword'
-      field.solr_local_parameters = {
-        qf: '$qf',
-        pf: '$pf'
-      }
     end
 
     config.add_search_field('title') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params.
       # field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
 
-      # :solr_local_parameters will be sent using Solr LocalParams
+      # :solr_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
       # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = {
-        qf: '$title_qf',
-        pf: '$title_pf'
+      field.solr_parameters = {
+        qf: '${title_qf}',
+        pf: '${title_pf}'
       }
     end
 
     config.add_search_field('publisher') do |field|
       field.qt = 'search'
       field.label = 'Publisher/Creator'
-      field.solr_local_parameters = {
-        qf: '$publisher_qf',
-        pf: '$publisher_pf'
+      field.solr_parameters = {
+        qf: '${publisher_qf}',
+        pf: '${publisher_pf}'
       }
     end
 
@@ -216,15 +212,15 @@ class CatalogController < ApplicationController
     config.add_search_field('subject') do |field|
       # field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
       field.qt = 'search'
-      field.solr_local_parameters = {
-        qf: '$subject_qf',
-        pf: '$subject_pf'
+      field.solr_parameters = {
+        qf: '${subject_qf}',
+        pf: '${subject_pf}'
       }
     end
 
     #  config.add_search_field('Institution') do |field|
     #   field.solr_parameters = { :'spellcheck.dictionary' => 'Institution' }
-    #   field.solr_local_parameters = {
+    #   field.solr_parameters = {
     #     :qf => '$Institution_qf',
     #     :pf => '$Institution_pf'
     #   }
