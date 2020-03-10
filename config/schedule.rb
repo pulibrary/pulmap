07c1f373fe843e4922abd56104f3f10ea03a98ae
Eliot Jordan <eliotj@princeton.edu>
2020-03-10 18:09:33 -0500

    Update whenever jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,9 +5,16 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-# Runs a daily rake task to harvest thumbnail images.
+# Run a daily rake task to harvest new thumbnail images.
 # These could be from new records or from records
 # where there was a previous error during harvesting.
 every :day, at: "11:00 PM", roles: [:db] do
+  rake "gblsci:images:harvest_new"
+end
+
+# Run a weekly rake task to harvest thumbnail in incomplete states.
+# These could be from new records or from records
+# where there was a previous error during harvesting.
+every :saturday, at: "11:00 AM", roles: [:db] do
   rake "gblsci:images:harvest_retry"
 end
