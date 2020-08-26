11443a357c72cdec797f260f2700a350a7144e85
Eliot Jordan <eliotj@princeton.edu>
2020-08-25 21:17:56 -0500

    Add new tool tips and help text

--- app/assets/javascripts/geoblacklight.js
+++ app/assets/javascripts/geoblacklight.js
@@ -3,11 +3,13 @@
 //= require geoblacklight/controls
 //= require geoblacklight/viewers
 //= require geoblacklight/modules/download
+//= require geoblacklight/modules/help_text
 //= require geoblacklight/modules/item
 //= require geoblacklight/modules/layer_opacity
 //= require geoblacklight/modules/metadata
 //= require geoblacklight/modules/metadata_download_button
 //= require geoblacklight/modules/relations
+//= require geoblacklight/modules/svg_tooltips
 //= require geoblacklight/modules/util
 //= require geoblacklight/downloaders
 
