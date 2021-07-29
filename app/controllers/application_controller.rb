25642727f8c0e677fe290bb04220ef435b2add86
Eliot Jordan <eliotj@princeton.edu>
2021-07-29 15:48:27 -0500

    Rescue RecordNotFound exceptions in the downloads controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from Blacklight::Exceptions::RecordNotFound do
+    render file: Rails.root.join('public', '404.html'), status: :not_found
+  end
+
   private
 
   def after_sign_in_path_for(_resource)
