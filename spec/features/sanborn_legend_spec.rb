24f27eefcc4bf4e5278f26fb83915d34e82ffb8d
Shaun Ellis <shaun@sdellis.com>
2022-01-13 14:59:36 -0500

    fixes rubocop complaint

--- spec/features/sanborn_legend_spec.rb
+++ spec/features/sanborn_legend_spec.rb
@@ -8,7 +8,7 @@ describe 'Sanborn Map Legend' do
     expect(page).to have_css('#sanborn-legend')
   end
 
-  it 'opens the modal containing the legend for Sanborn Maps', js:true do
+  it 'opens the modal containing the legend for Sanborn Maps', js: true do
     visit solr_document_path('princeton-1r66j405w')
     click_button('sanborn-legend-button')
     expect(page).to have_selector('#sanborn-legend-large', visible: true)
