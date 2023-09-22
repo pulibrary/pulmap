64a463fa84d02cff54ad8449c1681e6b5d487e90
Eliot Jordan <eliotj@princeton.edu>
2023-09-22 12:54:35 -0500

    Update capistrano git settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,6 @@ set :branch, ENV["BRANCH"] || "main"
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/opt/pulmap"
 
-# Default value for :scm is :git
-set :scm, :git
-
 # Default value for :format is :pretty
 # set :format, :pretty
 
