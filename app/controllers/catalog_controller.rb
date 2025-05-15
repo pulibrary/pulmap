c5d46892548ab769fe44880fec03b8bc47c3f179
Eliot Jordan <eliotj@princeton.edu>
2025-05-15 15:26:52 -0500

    Remove sms as the service is going away

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -232,7 +232,6 @@ class CatalogController < ApplicationController
     config.add_show_tools_partial(:bookmark, partial: "bookmark_control", if: :render_bookmarks_control?)
     config.add_show_tools_partial(:citation)
     config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
-    config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
 
     # Custom tools for GeoBlacklight
     config.add_show_tools_partial :web_services, if: proc { |_context, _config, options| options[:document] && (Settings.WEBSERVICES_SHOWN & options[:document].references.refs.map(&:type).map(&:to_s)).any? }
--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -13,9 +13,6 @@ class SolrDocument
   # Email uses the semantic field mappings below to generate the body of an email.
   SolrDocument.use_extension(Blacklight::Document::Email)
 
-  # SMS uses the semantic field mappings below to generate the body of an SMS email.
-  SolrDocument.use_extension(Blacklight::Document::Sms)
-
   # DublinCore uses the semantic field mappings below to assemble an OAI-compliant DC document
   # Semantic mappings of solr stored fields. Fields may be multi or
   # single valued. See Blacklight::Document::SemanticFields#field_semantics
