ecdfe0c5aae14341c609d5987adbc8858b68ae59
Eliot Jordan <eliot.jordan@gmail.com>
2026-01-12 14:24:27 -0600

    Update gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_12_201532) do
-
+ActiveRecord::Schema[7.2].define(version: 2022_05_12_201532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
