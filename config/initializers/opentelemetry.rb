6edcd1450430361ce8678a5219d191b0b47595db
Francis Kayiwa <kayiwa@pobox.com>
2026-05-14 07:32:49 -0400

    enable only the instrumentation that was working

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,7 +1,15 @@
 require "opentelemetry/sdk"
 require "opentelemetry/exporter/otlp"
-require "opentelemetry/instrumentation/all"
+require "opentelemetry/instrumentation/rack"
+require "opentelemetry/instrumentation/rails"
+require "opentelemetry/instrumentation/active_record"
+require "opentelemetry/instrumentation/action_pack"
 
 OpenTelemetry::SDK.configure do |c|
-  c.use_all # Enables all available instrumentations
+  c.service_name = ENV.fetch("OTEL_SERVICE_NAME", "pulmap")
+
+  c.use "OpenTelemetry::Instrumentation::Rails"
+  c.use "OpenTelemetry::Instrumentation::Rack"
+  c.use "OpenTelemetry::Instrumentation::ActiveRecord"
+  c.use "OpenTelemetry::Instrumentation::ActionPack"
 end
